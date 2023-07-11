811e1d4e1d18578dbe2cfca139f71ed26928896e
tomoya06 <peng4766@gmail.com>
2023-07-12 00:08:07 +0800

    feat: 笑死 写了错的

--- redux/src/store.ts
+++ redux/src/store.ts
@@ -1,32 +1,38 @@
-import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const rootReducer = createSlice({
   initialState: {
     count: 1,
-    input: '123123',
+    input: "123123",
   },
-  name: 'root',
+  name: "root",
   reducers: {
     addCount: (state) => {
-      state.count += 1;
+      return {
+        ...state,
+        count: state.count + 1,
+      };
     },
     editInput: (state, action: PayloadAction<string>) => {
-      state.input = action.payload;
+      return {
+        ...state,
+        input: action.payload,
+      };
     },
-  }
-})
+  },
+});
 
 const store = configureStore({
   reducer: {
     root: rootReducer.reducer,
-  }
+  },
 });
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
